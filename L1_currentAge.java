a77926ff7db9a9244c2b0b38ce6e0aeb3e616443
aayushii-singh <122150257+aayushii-singh@users.noreply.github.com>
2025-01-22 11:55:03 +0530

    Level1

---
+++ L1_currentAge.java
@@ -0,0 +1,10 @@
+public class L1_currentAge{
+public static void main(String args[]){
+int BirthYear=2000;
+int CurrentYear=2024;
+int age=CurrentYear- BirthYear;
+
+System.out.println("Harry's age in" + CurrentYear + "is" + age);
+
+} 
+}
