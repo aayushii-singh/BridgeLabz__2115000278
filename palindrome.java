02104ea89f9ffd08f9590727dbf4d80f564b4e2e
aayushii-singh <122150257+aayushii-singh@users.noreply.github.com>
2025-01-30 22:03:58 +0530

    palindrome1

---
+++ palindrome.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class palindrome {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String str = sc.nextLine();
+        sc.close();
+
+        int n = str.length();
+        for (int i = 0; i < n / 2; i++) {
+            if (str.charAt(i) != str.charAt(n - i - 1)) {
+                System.out.println("Not a Palindrome");
+                return;
+            }
+        }
+        System.out.println("Palindrome");
+    }
+}
